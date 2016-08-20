8da73032b363900ed72cee4d696d70ce36c9c370
mtipton81 <mtipton81@gmail.com>
2016-08-20 11:45:30 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 $('button').click(function(){
-       $('img').gadout('slow');
+       $('img').gadout('1000');
 });
 $('#button-two').click(function()}
+   $('img').fadein('1000');
          
 } 
   
